28f874e0ea0b106a5c76e1433141b07c6d05a640
Your Name <you@example.com>
2024-07-09 19:15:51 +0100

    init

---
+++ script.js
@@ -0,0 +1,38 @@
+const SKILLS =
+[
+    ['Angular','RxJs','TypeScrpt','Nx','NgRx','Tailwind','Bootstrap','Responsive Design','React',
+        'Angular Material','PrimeNG'
+    ],
+    [
+        'Node','ExpessJs','MongoDB'
+    ],
+    [
+        'Git','Docker','Jira','Sass','Webpack'
+    ]
+] 
+    
+
+
+window.addEventListener('DOMContentLoaded',onInit)
+
+function onInit() {
+    console.log("Welcome");
+    addFrontEndSkills()
+}
+
+function addFrontEndSkills() {
+    const holder =document.getElementById('skill-list')
+    SKILLS.forEach(skillSet =>{
+        const ul = document.createElement('ul')
+        skillSet.forEach(skill => addSkill(skill,ul))
+        holder.appendChild(ul)
+    })
+}
+
+
+function addSkill(skillName,element) {
+    const skillNode = document.createElement('li')
+    skillNode.classList.toggle('skill')
+    skillNode.innerText = skillName
+     return element.appendChild(skillNode)
+}
