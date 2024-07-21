1b11831734bef669057aa56320a66f8a8b45a1f1
Your Name <you@example.com>
2024-07-21 23:17:40 +0530

    experience and projects

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const SKILLS =
-[
-    ['Angular','RxJs','TypeScrpt','Nx','NgRx','Tailwind','Bootstrap','Responsive Design','React',
-        'Angular Material','PrimeNG'
-    ],
     [
-        'Node','ExpessJs','MongoDB'
-    ],
-    [
-        'Git','Docker','Jira','Sass','Webpack'
+        [
+            'Angular', 'RxJs', 'TypeScrpt', 'Nx', 'NgRx', 'Tailwind', 'Bootstrap', 'Responsive Design',
+            'Angular Material', 'PrimeNG'
+        ],
+        [
+            'React', 'React router', 'React hooks',
+        ],
+        [
+            'Node', 'ExpessJs', 'MongoDB'
+        ],
+        [
+            'Git', 'Docker', 'Jira', 'Sass', 'Webpack'
+        ]
     ]
-] 
-    
 
-
-window.addEventListener('DOMContentLoaded',onInit)
+window.addEventListener('DOMContentLoaded', onInit)
 
 function onInit() {
     console.log("Welcome");
@@ -21,18 +23,18 @@ function onInit() {
 }
 
 function addFrontEndSkills() {
-    const holder =document.getElementById('skill-list')
-    SKILLS.forEach(skillSet =>{
+    const holder = document.getElementById('skill-list')
+    SKILLS.forEach(skillSet => {
         const ul = document.createElement('ul')
-        skillSet.forEach(skill => addSkill(skill,ul))
+        skillSet.forEach(skill => addSkill(skill, ul))
         holder.appendChild(ul)
     })
 }
 
 
-function addSkill(skillName,element) {
+function addSkill(skillName, element) {
     const skillNode = document.createElement('li')
     skillNode.classList.toggle('skill')
     skillNode.innerText = skillName
-     return element.appendChild(skillNode)
+    return element.appendChild(skillNode)
 }
